84db80f7e76a092f9d814e4c3d7b3a72a83cc202
Myles Borins <mborins@us.ibm.com>
2016-01-14 17:33:49 -0800

    benchmark: update benchmarks for encode / decode

--- benchmark/buffers/buffer-base64-decode.js
+++ benchmark/buffers/buffer-base64-decode.js
@@ -1,15 +1,22 @@
-var assert = require('assert');
-var common = require('../common.js');
+'use strict';
+const SlowBuffer = require('buffer').SlowBuffer;
+const assert = require('assert');
+const common = require('../common.js');
 
-var bench = common.createBenchmark(main, {});
+const bench = common.createBenchmark(main, {
+  type: ['fast-zalloc', 'slow-zalloc', 'fast-alloc', 'slow-alloc']
+});
 
 function main(conf) {
+  const clazz = /^fast/.test(conf.type) ? Buffer : SlowBuffer;
+  const fn = /zalloc/.test(conf.type) ? clazz.zalloc : clazz.alloc;
+
   for (var s = 'abcd'; s.length < 32 << 20; s += s);
   s.match(/./);  // Flatten string.
   assert.equal(s.length % 4, 0);
-  var b = Buffer(s.length / 4 * 3);
-  b.write(s, 0, s.length, 'base64');
   bench.start();
+  var b = fn(s.length / 4 * 3);
+  b.write(s, 0, s.length, 'base64');
   for (var i = 0; i < 32; i += 1) b.base64Write(s, 0, s.length);
   bench.end(32);
 }
--- benchmark/buffers/buffer-base64-encode.js
+++ benchmark/buffers/buffer-base64-encode.js
@@ -1,14 +1,21 @@
-var common = require('../common.js');
+'use strict';
+const SlowBuffer = require('buffer').SlowBuffer;
 
-var bench = common.createBenchmark(main, {});
+const common = require('../common.js');
+const bench = common.createBenchmark(main, {
+  type: ['fast-zalloc', 'slow-zalloc', 'fast-alloc', 'slow-alloc']
+});
 
 function main(conf) {
-  var N = 64 * 1024 * 1024;
-  var b = Buffer(N);
-  var s = '';
-  for (var i = 0; i < 256; ++i) s += String.fromCharCode(i);
-  for (var i = 0; i < N; i += 256) b.write(s, i, 256, 'ascii');
+  const clazz = /^fast/.test(conf.type) ? Buffer : SlowBuffer;
+  const fn = /zalloc/.test(conf.type) ? clazz.zalloc : clazz.alloc;
+
+  const N = 64 * 1024 * 1024;
   bench.start();
-  for (var i = 0; i < 32; ++i) b.toString('base64');
+  const b = fn(N);
+  let s = '';
+  for (let i = 0; i < 256; ++i) s += String.fromCharCode(i);
+  for (let i = 0; i < N; i += 256) b.write(s, i, 256, 'ascii');
+  for (let i = 0; i < 32; ++i) b.toString('base64');
   bench.end(64);
 }
